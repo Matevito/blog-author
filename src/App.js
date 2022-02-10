863e532d97b8806da8b3f458cf2f67ebd6ea3a9f
Mateo <madiazt@unal.edu.co>
2022-02-10 15:52:18 -0500

    handleUser save token on localstorage and se4tup user state value on the app

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import Login from "./routes/Login";
 import Signin from "./routes/Signin";
 //import EditArticle from "./routes/EditArticle";
 //import CreateArticle from "./routes/CreateArticle";
+//todo: edit user profile data!
 
-// const API_PATH = "https://polar-depths-85779.herokuapp.com/"
+const axios = require("axios");
+const api = axios.create({
+  baseURL: "https://polar-depths-85779.herokuapp.com/apiv1/",
+  timeout: 1000
+});
 
 function App() {
+  // object structure: {id, username, token}
   const [user, setUser] = useState();
 
   useEffect(() => {
@@ -19,11 +25,30 @@ function App() {
     }
   }, []);
 
-  const handleUser = (userToken) => {
-    // todo set up userINfo as user and set up the token on the browser!
-    // call the api for user info!
-    setUser("loged in")
-    // set up token in browser
+  const handleUser = async (userToken) => {
+    // 1. set up jwt on localstorage
+    try { 
+      // 2. call api for user info of the jwt token}
+      const config = {
+        headers: {
+          "auth-token": userToken
+        }
+      }
+      const userInfo = await api.get("/whoami", config);
+      const userResponse = userInfo.data.data
+      
+      // cause everything is fine we can save the token on the storage
+      localStorage.setItem("blogToken", userToken)
+
+      // 3. set up the user object in the app.
+      setUser({
+        username: userResponse.username,
+        id: userResponse.id,
+        token: userToken
+      })
+    } catch (err) {
+      console.log(err.response.data)
+    }
   }
   return (
     <BrowserRouter>
