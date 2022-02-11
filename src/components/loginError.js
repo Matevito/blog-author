3fb20e0fd0154e422058a3d7bb4375aac0e20c92
Mateo <madiazt@unal.edu.co>
2022-02-10 21:30:00 -0500

    styled login form

--- src/components/loginError.js
+++ src/components/loginError.js
@@ -1,9 +1,14 @@
+import { Alert, AlertTitle, Stack } from "@mui/material";
+
 const LoginError = ({ error }) => {
     if (error === true) {
         return(
-            <div>
-                <h3>Theres a problem with the username or the password.</h3>
-            </div>
+            <Stack sx={{ width: '100%'}} spacing={2}>
+                <Alert variant="outlined" severity="error">
+                    <AlertTitle>Authentication error</AlertTitle>
+                    Theres a problem with the username or the password.
+                </Alert>
+            </Stack>
         )
     } else {
         return <></>
