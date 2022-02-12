5d4cecf64ff6e67551765025e73dca4ccc374a31
Mateo <madiazt@unal.edu.co>
2022-02-11 20:46:52 -0500

    added articleerror to createarticle

---
+++ src/components/articleError.js
@@ -0,0 +1,18 @@
+import { Alert, Stack } from "@mui/material";
+
+const articleError = ({ error }) => {
+    if (!error) {
+        return <></>
+    } else {
+        const msgError = error.error
+        return(
+            <Stack sx={{ width: '100%'}} spacing={2}>
+                <Alert variant="outlined" severity="error">
+                    {msgError}
+                </Alert>
+            </Stack>
+        )
+    }
+}
+
+export default articleError;
